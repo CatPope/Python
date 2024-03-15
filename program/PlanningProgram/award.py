670822e626658a051f7d645fc53ecd428ded3e89
CatPope <127297260+CatPope@users.noreply.github.com>
2024-03-15 15:13:16 +0900

    2024.03.15

--- program/PlanningProgram/award.py
+++ program/PlanningProgram/award.py
@@ -1,31 +1,25 @@
 import tkinter as tk
-from tkinter import ttk, messagebox, font
-import os
-from planning_chart import PlanningChart
-
+from tkinter import messagebox
+import random
 
 class Award:
-    def __init__(self, master, goal):
-        self.goal = goal
-
+    def __init__(self, master):
         self.master = master
-        self.master.title(goal+" 완료!")
-        self.master.geometry("250x150")
-        self.master.resizable(False, False)
+        self.master.title("Random Content")
 
+        self.random_button = tk.Button(master, text="Random Content", command=self.show_random_content)
+        self.random_button.pack(pady=10)
 
-        self.goals_combobox = ttk.Combobox(master, values=self.goals_list, width=23)
-        self.select_button = tk.Button(master, text="파일 열기", command=self.entered_goal)
-        self.delete_file_button = tk.Button(master, text="파일 삭제", command=self.delete_selected_file)
+    def show_random_content(self):
+        try:
+            with open("상.txt", "r", encoding="utf-8") as file:
+                lines = file.readlines()
+                random_line = random.choice(lines)
+                messagebox.showinfo("Random Content", random_line)
+        except Exception as e:
+            messagebox.showerror("Error", f"An error occurred: {e}")
 
-        self.goals_combobox.set("랜덤")
-        self.goals_combobox.pack(side="top", pady=10)
-        self.select_button.pack(side="left", expand=True, ipady=5)
-        self.delete_file_button.pack(side="left", expand=True, ipady=5)
-
-    def load_award(self):
-        if self.file_path:
-            with open(self.file_path, 'r') as file:
-                content = file.readlines()
-            for line in content:
-                self.plan_listbox.insert(tk.END, line.strip("\n"))
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = Award(root)
+    root.mainloop()
